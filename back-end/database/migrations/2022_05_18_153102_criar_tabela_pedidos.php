cfe32fa2f83854f024833044ea21e847b6e771d5
ReinaldoAttri <101647931+ReinaldoAttri@users.noreply.github.com>
2022-05-18 12:33:08 -0300

    Adicionando migrations

---
+++ back-end/database/migrations/2022_05_18_153102_criar_tabela_pedidos.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('codCliente');
+            $table->unsignedBigInteger('codProduto');
+            $table->timestamps();
+
+            $table->foreign('codCliente')->references('id')->on('clientes');
+            $table->foreign('codProduto')->references('id')->on('produtos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
