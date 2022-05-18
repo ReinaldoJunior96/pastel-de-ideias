5a348e0174762f37d91a7ddc1434abc94e00ff22
ReinaldoAttri <101647931+ReinaldoAttri@users.noreply.github.com>
2022-05-18 13:45:08 -0300

    Adicionando controllers

---
+++ back-end/app/Http/Controllers/ClienteController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    private $cliente;
+
+    public function __construct(Cliente $cliente)
+    {
+        $this->cliente = $cliente;
+    }
+
+    public function index()
+    {
+        return response()->json($this->cliente->all(), 200);
+    }
+
+    public function store()
+    {
+
+    }
+}
