38d083dcfdba96337dc68c26dd5dba03a2532007
ReinaldoAttri <101647931+ReinaldoAttri@users.noreply.github.com>
2022-05-18 17:31:01 -0300

    CRUDL pedidos

--- back-end/app/Http/Controllers/PedidoController.php
+++ back-end/app/Http/Controllers/PedidoController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pedido;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PedidoController extends Controller
 {
-    //
+    private Pedido $pedido;
+
+    public function __construct(Pedido $pedido)
+    {
+        $this->pedido = $pedido;
+    }
+
+    public function index(): JsonResponse
+    {
+        return response()->json($this->pedido->all(), 200);
+    }
+
+    public function show($id): JsonResponse
+    {
+        $pedidoEncontrado = $this->pedido->find($id);
+        return
+            empty($pedidoEncontrado) ?
+                response()->json('Pedido não encontrado', 500) :
+                response()->json($pedidoEncontrado, 200);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $this->pedido->nome = $request->nome;
+        $this->pedido->preco = $request->preco;
+        $this->pedido->pathImg = $request->pathImg;
+
+        return $this->pedido->save() ? response()->json('Success', 200) : response()->json('Error', 500);
+
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        $pedidoEncontrado = $this->pedido->find($id);
+        $pedidoEncontrado->nome = $request->nome;
+        $pedidoEncontrado->preco = $request->preco;
+        $pedidoEncontrado->pathImg = $request->pathImg;
+
+        return $pedidoEncontrado->save() ? response()->json('Success', 200) : response()->json('Error', 500);
+
+    }
+
+    public function delete($id): JsonResponse
+    {
+        $pedidoEncontrado = $this->pedido->find($id);
+        if ($pedidoEncontrado == null) {
+            return response()->json('Pedido não encontrado', 200);
+        }
+        return $pedidoEncontrado->delete() ?
+            response()->json('Success', 200) : response()->json('Error', 500);
+    }
+
+
 }
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\PedidoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(ClienteController::class)->group(function(){
+Route::controller(ClienteController::class)->group(function () {
     Route::get('/clientes', 'index')->name('listar.clientes');
     Route::get('/clientes/{id}', 'show')->name('listar.unico.cliente');
     Route::post('/clientes', 'store')->name('criar.cliente');
     Route::put('/clientes/alterar/{id}', 'update')->name('update.cliente');
     Route::delete('/clientes/deletar/{id}', 'delete')->name('delete.cliente');
+});
+
+Route::controller(PedidoController::class)->group(function () {
+    Route::get('/pedidos', 'index')->name('listar.pedidos');
+    Route::get('/pedidos/{id}', 'show')->name('listar.unico.pedido');
+    Route::post('/pedidos', 'store')->name('criar.pedido');
+    Route::put('/pedidos/alterar/{id}', 'update')->name('update.pedido');
+    Route::delete('/pedidos/deletar/{id}', 'delete')->name('delete.pedido');
 });
