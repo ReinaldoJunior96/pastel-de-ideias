71253367ff2f94fc35345f1be78753077b14edaf
ReinaldoAttri <101647931+ReinaldoAttri@users.noreply.github.com>
2022-05-24 11:37:16 -0300

    Limpando formulário

--- front-end/src/components/Formulario.vue
+++ front-end/src/components/Formulario.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="flex justify-center relative ">
+  <div class="flex justify-center relative mb-10">
     <div class="bg-white rounded-2xl w-full phone:w-8/12 tablet:w-8/12 laptop:w-8/12 desktop:w-8/12 shadow-xl ">
-      <div class="flex items-center phone:gap-0 laptop:flex-row flex-col bg-optional-color rounded-tl-2xl rounded-tr-2xl py-3 px-10 ">
+      <div
+          class="flex items-center phone:gap-0 laptop:flex-row flex-col bg-optional-color rounded-tl-2xl rounded-tr-2xl py-3 px-10 ">
         <div class="flex flex-1 justify-center laptop:justify-start text-center laptop:text-left">
           <h1 class="text-secondary-color text-base font-bold">Monte aqui o seu cardápio. O que está esperando?</h1>
         </div>
         <div class="flex justify-center laptop:justify-end items-center gap-3 py-3">
           <span class="text-primary-color">Comida</span>
           <label class="switch">
-            <input v-model="tipo" @change="tipoComida()" type="checkbox">
+            <input v-model="pedido.tipo" @change="tipoComida()" type="checkbox">
             <span class="slider round"></span>
           </label>
           <span class="text-primary-color">Bebida</span>
@@ -31,20 +32,35 @@
                     class="outline-0 border rounded-lg border-primary-color w-full px-2" rows="4"
                     cols="50"></textarea>
         </div>
-        <div class="">
+        <div class="w-full">
           <picture-input
-              @change="fototeste"
-              width="500"
-              height="400"
-              margin="10"
-              accept="image/jpeg,image/png">
+              id="foto"
+              ref="pictureInput"
+              width="300"
+              height="300"
+              margin="16"
+              accept="image/jpeg,image/png"
+              size="10"
+              :removable="true"
+              button-class="btn"
+              :custom-strings="{
+                upload: '<h1>Bummer!</h1>',
+                drag: 'Arraste ou clique para inserir imagem',
+                 change: 'Alterar Foto',
+                 remove: 'Remover Foto',
+              }"
+              @change="onChange">
           </picture-input>
         </div>
       </div>
       <div class="flex flex-col laptop:flex-row justify-center items-center gap-3 laptop:-mb-4 mb-5">
-        <button class="bg-red-500 py-2 px-10 text-white font-bold rounded-3xl uppercase order-last laptop:order-first w-1/2 laptop:w-3/12">Limpar</button>
+        <button @click="limparPedido"
+                class="bg-red-500 py-2 px-10 text-white font-bold rounded-3xl uppercase order-last laptop:order-first w-1/2 laptop:w-3/12">
+          Limpar
+        </button>
         <button @click="salvarPedido"
-                class="bg-amber-400 py-2 px-10 font-bold rounded-3xl text-sm text-secondary-color uppercase w-1/2 laptop:w-3/12">Cadastrar
+                class="bg-amber-400 py-2 px-10 font-bold rounded-3xl text-sm text-secondary-color uppercase w-1/2 laptop:w-3/12">
+          Cadastrar
         </button>
       </div>
     </div>
@@ -61,40 +77,50 @@ export default {
   },
   data() {
     return {
-      tipo: Boolean,
       image: '',
       pedido: {
         titulo: '',
+        tipo: Boolean,
         sabor: '',
         valor: 0,
         descricao: '',
+        image: ''
       }
     }
   },
   methods: {
     salvarPedido() {
-      this.$store.commit('salvarPedido');
+      this.$store.commit('salvarPedido', this.pedido);
+
     },
-    fototeste() {
-      console.log("Picture changed!")
+    limparPedido() {
+      this.pedido.titulo = ''
+      this.pedido.tipo = false
+      this.pedido.sabor = ''
+      this.pedido.valor = 0
+      this.pedido.descricao = ''
+      this.pedido.image = ''
+      const element = document.querySelector('.btn-secondary')
+      element.click();
+
     },
     onChange(image) {
-      console.log('New picture selected!')
+      console.log('Nova imagem selecionada')
       if (image) {
-        console.log('Picture loaded.')
-        this.image = image
-        console.log(this.image)
+        console.log('Imagem Carregada.')
+        this.pedido.image = image
       } else {
         console.log('FileReader API not supported: use the <form>, Luke!')
       }
     }
   },
-
 }
 </script>
 
-<style scoped>
-h1 * {
-  font-size: 2px;
+<style>
+
+.picture-inner-text[data-v-431cb064] {
+  font-size: 12px !important;
 }
+
 </style>
--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -3,11 +3,18 @@ import { createStore } from 'vuex'
 export default createStore({
     state: {
         displayPedido: 'hidden',
+        pedido:{
+            titulo: '',
+            sabor: '',
+            valor: 0,
+            descricao: '',
+        }
     },
     mutations: {
-        salvarPedido(){
-            console.log('reinaldo')
+        salvarPedido(state, payload){
             this.state.displayPedido = 'block';
+            console.log(payload)
+            //this.state.pedido.titulo =
         }
     }
 })
